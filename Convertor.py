5ae17191b09a14dcbbd7fd6dc41e3f6ee15201da
m8tan <alt6526@gmail.com>
2025-10-16 12:23:09 +0300

    Added functions

--- Convertor.py
+++ Convertor.py
@@ -10,12 +10,29 @@ def Convert_List_To_Tuple(Input_List):
     return Output_Tuple
 def Reverse_List(Input_List):
     return Input_List[::-1]
+def Find_List_Average(Input_List, Accuracy):
+    Total = 0
+    Yes_Int = 0
+    for item in Input_List:
+        if isinstance(item, (int, float)):
+            Total += item
+            Yes_Int += 1
+    if Yes_Int == 0:
+        return "No numbers in given list"
+        #raise Exception("No numbers in given list")
+    return round(Total / Yes_Int, Accuracy)
+    
+        
+    
+
 if __name__ == "__main__":
 
-    Test_List = [1, 2, 4, 8, 6, 3, 7, 2]
-    Test_Dict = Convert_List_To_Dictionary(Test_List)
-    Test_Tuple = Convert_List_To_Tuple(Test_List)
-    Eversed_List = Reverse_List(Test_List)
-    print(Test_Dict)
-    print(Test_Tuple)
-    print(Eversed_List)
+    Test_List = [1, 2, 4, 8, "a", 3, 7, 2]
+    bs = ["a", "b", "c"]
+    try:
+        Avg = Find_List_Average(bs, 2)
+        print(Avg)
+    except Exception as e:
+        print(e)
+    
+    
